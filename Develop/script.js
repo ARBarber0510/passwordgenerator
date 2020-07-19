2a3661c3b0cf5c175d2eb54f54943cd57cfddce8
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-07-19 17:37:54 -0500

    added password character and declaration variables.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","<",">",".","?","/"];
+
+var confirmLength = "";
+var confirmNumbers; 
+var confirmLowerAlpha;
+var confirmUpperAlpha;
+var confirmSpecialChar;
 
 // Write password to the #password input
 function writePassword() {
